16d8c2200bb68e7042b27027f133472dffa83a48
Sebastian Cato <seb.cato@gmail.com>
2016-07-17 21:43:05 +0200

    interp_test: added tests

--- interp_test.c
+++ interp_test.c
@@ -7,7 +7,10 @@ int main() {
 	interp_t *interp;
 
 	setenv("LUA_PATH", "?.lua;?", 1);
+	interp_free(NULL); /* should not crash */
 	interp = interp_new();
+	EXPECT(*interp_errstr(interp) == '\0',
+			"non-empty error string after allocation");
 	EXPECT(interp != NULL, "interp was NULL");
 	EXPECT(interp_load(interp, "IAHUHF.EAHFE.FHE.FH") == BIRK_ERROR,
 			"unexpected success of nonexistent library");
