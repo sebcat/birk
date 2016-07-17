468e641125f7b7c70979c783ab3b533fd65ba24c
Sebastian Cato <seb.cato@gmail.com>
2016-07-17 23:02:51 +0200

    birk: added init.lua

---
+++ birk/init.lua
@@ -0,0 +1,10 @@
+local birk = {
+  created = "2016-07-17",
+  description ="birk init code",
+  copyright = "Sebastian Cato",
+  license = "ISC License",
+}
+
+birk.irc = require "birk.irc"
+
+return birk
