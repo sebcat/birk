5c0962633db6c39bcffbb2e05959895bc4b1c31d
Sebastian Cato <seb.cato@gmail.com>
2016-08-01 16:27:21 +0200

    lua: Added library wrappers for birk IPC functions

--- birk/init.lua
+++ birk/init.lua
@@ -7,4 +7,19 @@ local birk = {
 
 birk.irc = require "birk.irc"
 
+-- birk-agent registers a userdata object for IPC with the host process. If it
+-- doesn't exist we're not started from birk-agent and can't continue.
+local ipc = __birkipc
+if ipc == nil then
+  error("birk IPC not present")
+end
+
+-- Initializes a connection in the parent process. Connections are established
+-- asyncronously by the parent and the interpreter is notified on success or
+-- failure
+function birk.connect_tcp(host,port)
+  return ipc:connect_tcp(host,port)
+end
+
+ipc:ready() -- tell parent we're done loading
 return birk
